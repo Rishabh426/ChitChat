43dd5e42a222036352134dc167363b951c737d89
Rishabh426 <sharmarishabh9900@gmail.com>
2026-04-11 16:13:17 +0530

    in store

---
+++ BE/src/store/Store.ts
@@ -0,0 +1,17 @@
+export type userId = string;
+
+export interface Chat {
+  id: string;
+  userId: userId;
+  name: string;
+  message: string;
+  upvotes: userId[]; // who has upvoted what
+}
+
+export abstract class Store {
+  constructor() {}
+  initRoom(roomId: string) {}
+  getChats(roomId: string, limit: number, offset: number) {}
+  addChat(userId: userId, name: string, roomId: string, message: string) {}
+  upvote(userId: userId, roomId: string, chatId: string) {}
+}
